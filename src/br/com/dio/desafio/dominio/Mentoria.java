8c2502885e2092b3c41a2d2bfed252c19f20b285
VictorYoshioMaeda12 <victor.yoshio12@hotmail.com>
2021-11-27 12:07:23 -0300

    Last commit

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,32 +2,19 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
 	
 	public Mentoria() {
 		
 	}
 
-	public String getTitulo() {
-		return titulo;
+	@Override
+	public double calcularXp() {
+		// TODO Auto-generated method stub
+		return XP_PADRAO +20;
 	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
 	public LocalDate getData() {
 		return data;
 	}
@@ -38,7 +25,9 @@ public class Mentoria {
 
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo()+ ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
+
+	
 
 }
